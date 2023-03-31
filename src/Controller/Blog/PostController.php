9084b0f262a62296f63fb812d9d4ab55c2f94ebf
damizkrli <karli.damien@gmail.com>
2023-03-31 23:56:36 +0200

    Mise en place de la pagination

--- src/Controller/Blog/PostController.php
+++ src/Controller/Blog/PostController.php
@@ -3,16 +3,29 @@
 namespace App\Controller\Blog;
 
 use App\Repository\Post\PostRepository;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\Paginator;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
     #[Route('/', name: 'post.index', methods: ['GET'])]
-    public function index(PostRepository $postRepository): Response
+    public function index(
+        PostRepository $postRepository,
+        PaginatorInterface $pagination,
+        Request $request
+    ): Response
     {
-        $post = $postRepository->findPublished();
+        $data = $postRepository->findPublished();
+        $post = $pagination->paginate(
+            $data,
+            $request->query->getInt('page', 1),
+            9
+        );
 
         return $this->render('pages/blog/index.html.twig', [
             'posts' => $post
